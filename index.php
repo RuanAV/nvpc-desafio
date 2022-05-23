5e2e10d43640ff282545b5f347bedcbbea96c944
Ruan <rav3@cin.ufpe.br>
2022-05-23 20:15:28 -0300

    adding page that show repository

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NVPC|Desafio</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <!-- <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24" class="d-inline-block align-text-top"> -->
+                NVPC | Desafio
+            </a>
+        </div>
+    </nav>
+    <div class="content">
+        <div class="container-fluid " >
+            <div class="col-lg-10 mx-auto">
+                <table class="table table-striped">
+                    <thead>
+                        <tr class="text-center">
+                            <th colspan='3'>Repositórios do GITHUB</th>
+                        </tr>
+                        <tr>
+                            <th>id</th>
+                            <th>nome</th>
+                            <th>Ultimo Commit</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                type: 'GET',
+                url: 'api/repositorios.php',
+                success: function(response) {
+                    response.map(function(data) {
+                        var html = `
+                        <tr>
+                            <td>`+data.id+`</td>
+                            <td>`+data.name+`</td>
+                            <td>`+data.updated_at+`</td>
+                        </tr>
+                        `;
+
+                        $('table').append(html);
+                    });
+                }
+            })
+        });
+    </script>
+</body>
+</html>
